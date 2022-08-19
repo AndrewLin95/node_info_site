84dd020a35a83509d5befb9de084fdcf7d796620
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-19 16:57:55 -0400

    have node parse the url and update pages based on the requested url. had a homepage with path / display the homepage html file

--- index.js
+++ index.js
@@ -3,13 +3,38 @@ const url = require('url');
 const fs = require('fs');
 
 const port = 8080
+const page404 = fs.readFileSync("page404.html", "utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
+});
 
+const homePage = fs.readFileSync('index.html',"utf-8", (err, data) => {
+    if (err) throw err;
+    return data;
+});
 
 const server = http.createServer((req, res) => {
   const myURL = url.parse(req.url, true);
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/html')
-  res.end(`<h1>Hello, World ${myURL}</h1>`)
+  let filename = '.' + myURL.pathname;
+
+  if (myURL.pathname === '/'){
+    fs.readFile(filename, function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(homePage);
+        return res.end();
+    })
+  } else {
+    fs.readFile(filename, function(err, data) {
+      if (err) {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write(page404);
+        return res.end(myURL.pathname);
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    })
+  }
 });
 
 server.listen(port, () => {
